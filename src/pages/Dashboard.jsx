196cd074904ae4c23f1aa3a28a53aac829077888
angga <68156108+anggavirgi@users.noreply.github.com>
2023-11-13 10:01:33 +0700

    final

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 export const Dashboard = () => {
   return (
     <div className='w-full h-screen p-2 flex flex-col gap-2'>
-      <div className='w-1/2 sm:w-full md:w-full lg:w-full xl:w-full bg-red-500 font-bold text-xl flex justify-center items-center py-5'>Merah</div>
+      <div className='flex gap-2'>
+        <div className='block w-2/3 sm:hidden md:hidden lg:hidden xl:hidden'></div>
+        <div className='w-1/3 sm:w-full md:w-full lg:w-full xl:w-full bg-red-500 font-bold text-xl flex justify-center items-center py-5'>Merah</div>
+      </div>
       <div className='flex flex-col xl:flex-row h-full w-full gap-2'>
-        <div className='bg-purple-500 h-full w-full xl:w-1/2 font-bold text-xl flex justify-center items-center xl:order-first md:order-last'>Ungu</div>
-        <div className='h-full md:w-full xl:w-1/2 flex flex-col gap-2'>
+        <div className='bg-purple-500 h-2/5 w-full xl:w-1/2 xl:h-full font-bold text-xl flex justify-center items-center xl:order-first md:order-last'>Ungu</div>
+        <div className='h-3/5 md:w-full xl:w-1/2 xl:h-full flex flex-col gap-2'>
           <div className='bg-green-500 h-full xl:h-1/2 font-bold text-xl flex justify-center items-center order-last sm:order-first md:order-first lg:order-first xl:order-first'>Hijau</div>
           <div className='flex h-1/2 gap-2'>
             <div className='bg-blue-500 w-1/2 font-bold text-xl flex justify-center items-center order-last sm:order-first md:order-first lg:order-first xl:order-first'>Biru</div>
